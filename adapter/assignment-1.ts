fa18b13591b06d0ed6bf40a2991cbe82ced916fe
sarbear28 <138033132+sarbear28@users.noreply.github.com>
2026-01-19 04:08:35 +0000

    Fix listBooks adapter to return fetched books

--- adapter/assignment-1.ts
+++ adapter/assignment-1.ts
@@ -10,21 +10,25 @@ export interface Book {
 
 // If you have multiple filters, a book matching any of them is a match.
 async function listBooks(filters?: Array<{ from?: number, to?: number }>): Promise<Book[]> {
-  //     const query = filters?.map(({ from, to }, index) => {
-  //     let result = ''
-  //     if (typeof from === 'number') {
-  //       result += `&filters[${index}][from]=${from}`
-  //     }
-  //     if (typeof to === 'number') {
-  //       result += `&filters[${index}][to]=${to}`
-  //     }
-  //     return result
-  //   }).join('&') ?? ''
+       const query = filters?.map(({ from, to }, index) => {
+       let result = ''
+       if (typeof from === 'number') {
+         result += `&filters[${index}][from]=${from}`
+       }
+       if (typeof to === 'number') {
+         result += `&filters[${index}][to]=${to}`
+       }
+       return result
+     }).join('&') ?? ''
 
   // Use the browser fetch API to make a request to the /books endpoint with the constructed query string.
-  // fetch(`http://localhost:3000/books?${query}`);
+ let result = await fetch(`http://localhost:3000/books?${query}`);
+ if (result.ok) {
+  return (await result.json()) as Book[]
+ } else {
+  throw new Error('Failed to fetch books');
+ }
 
-  throw new Error("Todo: implement function listBooks")
 }
 
 const assignment = "assignment-1";
--- src/books/lists.ts
+++ src/books/lists.ts
@@ -10,16 +10,16 @@ listRouter.get('/books', async (ctx) => {
   try {
     let bookList = readBooksFromJsonData();
 
-    // Todo: Uncomment to Apply filters
-    // if (filters && Array.isArray(filters) && filters.length > 0) {
-    //   if (!validateFilters(filters)) {
-    //     ctx.status = 400;
-    //     ctx.body = { error: 'Invalid filters. Each filter must have valid "from" and "to" numbers where from <= to.' };
-    //     return;
-    //   }
+     
+     if (filters && Array.isArray(filters) && filters.length > 0) {
+       if (!validateFilters(filters)) {
+         ctx.status = 400;
+         ctx.body = { error: 'Invalid filters. Each filter must have valid "from" and "to" numbers where from <= to.' };
+         return;
+       }
 
-    //   bookList = filterBooks(bookList, filters);
-    // }
+       bookList = filterBooks(bookList, filters);
+     }
 
     ctx.body = bookList;
   } catch (error) {
