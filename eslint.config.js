c394779c5f7686c2e777d88a894de9f29d02e117
sarbear28 <138033132+sarbear28@users.noreply.github.com>
2026-02-01 00:56:20 +0000

    added code to eslint.config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,15 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
